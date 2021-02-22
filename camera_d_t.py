669ed836c99f95f61d5a5b9d1aef44e89eeeeca6
listar2000 <lisd.star2015@outlook.com>
2021-02-22 00:11:14 -0800

    calibration functionality added

--- camera_d_t.py
+++ camera_d_t.py
@@ -5,6 +5,7 @@ import pyrealsense2 as rs
 import math as m
 import numpy as np
 import sys
+import time
 
 CAM_CONFIG = {
     "image_w": 848,
@@ -12,9 +13,11 @@ CAM_CONFIG = {
     "framerate": 30,
     "aruco_dict": aruco.DICT_5X5_250,
     "aruco_thres": 10,
-    "aruco_block_size": 0.154,
+    "aruco_block_size": 0.1592,
     "use_default_cam2cam": True,
-    "detect_mode": False
+    "detect_mode": False,
+    # calibrate threshold in degree
+    "calibrate_threshold": 1
 }
 
 class RS_D_T(object):
@@ -47,6 +50,7 @@ class RS_D_T(object):
             raise ConnectionError(f"Error {e}. Pipeline Initialization Error")
  
         self.running = True
+        self.calibrated = False
 
         camera_intr = self.prof_d.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
         self.rgb_mtx = np.array([[camera_intr.fx, 0, camera_intr.ppx], [0, camera_intr.fy, camera_intr.ppy], [0, 0, 1]])
@@ -59,10 +63,76 @@ class RS_D_T(object):
         self.block_size = CAM_CONFIG['aruco_block_size']
         self.use_default_cam2cam = CAM_CONFIG['use_default_cam2cam']
         self.detect_mode = CAM_CONFIG['detect_mode']
+        self.calibrate_thres = CAM_CONFIG['calibrate_threshold']
 
-        self.c2c, self.c2m = None, None
+        self.t2d, self.d2m, self.t2m = None, None, None
 
         self.logger.info("Camera Initiated")
+
+    """
+    Important function that perform calibration check before start detecting ARUCO marker and getting
+    transformation matrix
+    """
+    def calibrate(self, show_img=False):
+        self.logger.debug("Calibration process starts")
+
+        while not self.calibrated:
+            frame_t = self.pipe_t.wait_for_frames()
+            frame_d = self.pipe_d.wait_for_frames()
+
+            pose_frame = frame_t.get_pose_frame()
+            color_frame = frame_d.get_color_frame()
+
+            img = self.color_frame_data(color_frame)
+            t_tvec, t_rvec = self.pose_frame_data(pose_frame)
+
+            rpy = self.rvec_to_rpy(t_rvec)
+
+            if show_img:
+                t_flag = max(rpy[0], rpy[2]) > self.calibrate_thres
+                if t_flag:
+                    boo, bar = round(rpy[0], 3), round(rpy[2], 3)
+                    t_msg = "roll: {}, yall: {} | expected: both < 1".format(boo, bar)
+                else:
+                    t_msg = "calibration angles are correct"
+
+                d2m, tvec, rvec = self.get_trans_mat(img)
+
+                d_flag = d2m is None
+                if d_flag:
+                    d_msg = "aruco marker not detected"
+                else:
+                    d_msg = "aruco marker detected"
+
+                cv2.putText(img, t_msg, (0, 40), self.font, 1, (0,255,255), 2, self.line_type)
+                cv2.putText(img, d_msg, (0, 64), self.font, 1, (255,255,0), 2, self.line_type)
+                cv2.imshow("frame", img)
+                cv2.waitKey(100)
+
+                if not (t_flag or d_flag):
+                    self.d2m = d2m
+                    self.t2d = self.cam2cam(t_rvec, t_tvec)
+                    self.t2m = self.d2m @ self.t2d 
+                    self.calibrated = True
+                    print("calibration success: matrices loaded")
+            else:
+                # sleep for the command line mode since 
+                time.sleep(0.5)
+                if max(rpy[0], rpy[2]) > self.calibrate_thres:
+                    print("calibration fails:")
+                    print("roll: {}, yall: {} | expected: both < 1".format(rpy[0], rpy[2]))
+                    continue
+
+                d2m, tvec, rvec = self.get_trans_mat(img)
+                if d2m is None:
+                    print("calibration fails: \naruco marker not detected")
+                else:
+                    self.d2m = d2m
+                    self.t2d = self.cam2cam(t_rvec, t_tvec)
+                    self.t2m = self.d2m @ self.t2d 
+                    self.calibrated = True
+                    print("calibration success: matrices loaded")
+
 
     def get_intrinsics(self):
         return {
@@ -81,7 +151,15 @@ class RS_D_T(object):
     def stop_detect(self):
         self.detect_mode = False
 
+    """
+    returns the [x, y, z] global coordiante of the vehicle (camera) in the map
+    default format is a 3x1 np array, return np.nan when the pipeline errors
+    """
     def poll(self):
+        if not self.calibrated:
+            self.logger.error("Camera not calibrated yet")
+            return np.nan
+
         try:
             frame_d = self.pipe_d.wait_for_frames()
             frame_t = self.pipe_t.wait_for_frames()
@@ -89,36 +167,23 @@ class RS_D_T(object):
             pose_frame = frame_t.get_pose_frame()
             color_frame = frame_d.get_color_frame()
 
-            if color_frame:
-                img = np.asanyarray(color_frame.get_data())
-            else:
-                return None
+            img = self.color_frame_data(color_frame)
+            t_tvec, t_rvec = self.pose_frame_data(pose_frame)
 
-            if pose_frame:
-                data = pose_frame.get_pose_data()
-                t = data.translation
-                t_tvec = np.array([t.x, t.y, t.z, 1])
-                t_rvec = data.rotation
-            else:
-                return None
-
-            if self.c2m is None:
-                c2m, tvec, rvec = self.get_trans_mat(img)
-                if c2m is not None:  
-                    self.c2m = c2m
-                    self.c2c = self.cam2cam(t_rvec)
-                    self.location = (self.c2m @ self.c2c @ t_tvec)[:3]
-            else:
-                self.location = (self.c2m @ self.c2c @ t_tvec)[:3]
+            self.location = (self.t2m @ t_tvec)[:3]
 
             if self.show_gui:
                 if self.detect_mode:
                     c2m, tvec, _ = self.get_trans_mat(img)
                     if tvec is not None:
-                        cv2.putText(img, str((c2m @ [0,0,0,1])[:3]), (0, 64), self.font, 1, (255,255,0), 2, self.line_type)  
-                        
-                cv2.putText(img, str(self.location), (0, 40), self.font, 1, (0,255,255), 2, self.line_type)
-
+                        rounded = np.round((c2m @ [0,0,0,1])[:3], decimals=3)
+                        cv2.putText(img, str(rounded), (0, 64), self.font, 1, (255,255,0), 2, self.line_type)  
+                
+                rounded = np.round(self.location, decimals=3)
+                cv2.putText(img, str(rounded), (0, 40), self.font, 1, (0,255,255), 2, self.line_type)
+                
+                # cv2.putText(img, str(t_tvec), (0, 40), self.font, 1, (0,255,255), 2, self.line_type)
+                # cv2.putText(img, str(self.rvec_to_rpy(t_rvec)), (0, 64), self.font, 1, (255,255,0), 2, self.line_type)
                 cv2.imshow("frame", img)
                 key = cv2.waitKey(100)
                 key_ord = key & 0xFF
@@ -132,8 +197,28 @@ class RS_D_T(object):
 
         except Exception as e:
             logging.error(e)
-            self.stop()
-            return None
+            return np.nan
+
+    def color_frame_data(self, color_frame):
+        return np.asanyarray(color_frame.get_data())
+    
+    def pose_frame_data(self, pose_frame):
+        data = pose_frame.get_pose_data()
+        t = data.translation
+        t_tvec = np.array([t.x, t.y, t.z, 1])
+        r = data.rotation
+        t_rvec = np.array([r.x, r.y, r.z, r.w])
+        return t_tvec, t_rvec
+
+    """
+    Helper function that turns a rotation vector into roll, pitch, and yaw angles
+    """
+    def rvec_to_rpy(self, rvec):
+        x, y, z, w = tuple(rvec)
+        pitch =  -m.asin(2.0 * (x*z - w*y)) * 180.0 / m.pi
+        roll  =  m.atan2(2.0 * (w*x + y*z), w*w - x*x - y*y + z*z) * 180.0 / m.pi
+        yaw   =  m.atan2(2.0 * (w*z + x*y), w*w + x*x - y*y - z*z) * 180.0 / m.pi
+        return np.array([roll, pitch, yaw])
 
     """
     This is a transformation from the d-camera's coord system to the marker's (world) system
@@ -157,39 +242,44 @@ class RS_D_T(object):
     2) there's still some minor translation between their coordinate systems, which will
     be implemented later :TODO @Star
     """
-    def cam2cam(self, t_rvec):
-        # no tuning of the t camera rotation
-        if self.use_default_cam2cam:
-            return np.array([1,0,0,0,
+    def cam2cam(self, t_rvec, t_tvec):
+        base_c2c = np.array([1,0,0,0,
                              0,-1,0,0,
                              0,0,-1,0,
                              0,0,0,1]).reshape((4, 4))
+        # no tuning of the t camera rotation
+        if self.use_default_cam2cam:
+            return base_c2c
         else:
             trans_mat = np.zeros((4, 4))
-            trans_mat[:3,:3] = cv2.Rodrigues(t_rvec)[0]
-            trans_mat[3,3] = 1
+            trans_mat[:3,:3] = cv2.Rodrigues(t_rvec[:3])[0]
+            trans_mat[3,:] = t_tvec
             trans_mat = np.linalg.inv(trans_mat)
-            return trans_mat
+            return trans_mat @ base_c2c
 
     def get_trans_mat(self, img):
         corners, ids, _ = aruco.detectMarkers(img, self.aruco_dict, parameters=self.parameters)
         if ids: # there's at least one aruco marker in sight
             rvec, tvec ,_ = aruco.estimatePoseSingleMarkers(corners, self.block_size, self.rgb_mtx, self.rgb_dist)
-            c2m = self.cam2marker(rvec, tvec)
+            d2m = self.cam2marker(rvec, tvec)
 
             if self.show_gui:
                 aruco.drawAxis(img, self.rgb_mtx, self.rgb_dist, rvec[0], tvec[0], 0.01)
                 aruco.drawDetectedMarkers(img, corners)
 
-            return c2m, tvec, rvec
+            return d2m, tvec, rvec
         else:
             return None, None, None
 
 if __name__ == '__main__':
     camera = RS_D_T()
     counter = 0
+    camera.calibrate(show_img=True)
     while True:
         loc = camera.poll()
+        if np.isnan(loc).all():
+            camera.stop()
+            break
         if counter % 20 == 0:
             print(loc)
         counter += 1
